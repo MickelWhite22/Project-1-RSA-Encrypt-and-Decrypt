d81ad6738ffd8967a78f53db27930f1bd33eac8c
MickelWhite22 <nolimitjuniorr@gmail.com>
2024-07-30 19:32:30 -0400

    Update Encrypting Public Key.py

--- Encrypting Public Key.py
+++ Encrypting Public Key.py
@@ -1,16 +1,17 @@
+# Importing RSA library.
 import rsa
 
-# Specifying the length of the generated keys in bits
+# Specifying the length of the generated keys in bits.
 public_key, private_key = rsa.newkeys(1024)
 
-#Create separate files where the generated keys will be held
+#Create separate files where the generated keys will be held.
 # with open("public.pem", "wb") as f:
    # f.write(public_key.save_pkcs1("PEM"))
     
 # with open("private.pem", "wb") as f:
    # f.write(private_key.save_pkcs1("PEM"))
 
-# After the files are created to prevent the keys from regenerating I deleted the previous code and using the same algorithm so the keys will be in my script 
+# After the files are created to prevent the keys from regenerating, I delete the previous code and use the same algorithm so the keys will be in my script. 
 with open("public.pem", "rb") as f:
    public_key = rsa.PublicKey.load_pkcs1(f.read())
     
