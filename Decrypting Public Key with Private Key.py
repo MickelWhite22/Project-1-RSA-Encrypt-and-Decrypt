104493b5830254aa1107b702bcb1b211c7389f21
MickelWhite22 <nolimitjuniorr@gmail.com>
2024-08-01 00:08:26 -0400

    Update Decrypting Public Key with Private Key.py

--- Decrypting Public Key with Private Key.py
+++ Decrypting Public Key with Private Key.py
@@ -1,6 +1,6 @@
 import rsa
 
-public_key, private_key = rsa.newkeys(1024)
+public_key, private_key = rsa.newkeys(2048)
 
 with open("public.pem", "rb") as f:
    public_key = rsa.PublicKey.load_pkcs1(f.read())
